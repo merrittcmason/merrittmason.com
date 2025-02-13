218fbe9f26ddb2efce81d43af49f6e38a6d36d80
PartialArc <merrittcmason@gmail.com>
2025-02-13 16:10:41 -0500

    Added new resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Menu, X, Moon, Sun, Github, Linkedin, Mail, Download, ExternalLink } from 'lucide-react';
+import pdfUrl from './merrittmason.pdf';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -74,7 +75,7 @@ function App() {
               shrink-0 flex-none
               ${isDarkMode ? 'border-gray-700' : 'border-gray-200'}">
               <img
-                src="/src/Headshot.png"
+                src="https://github.com/PartialArc/Resume-Website/blob/c97e04db5bc2f015f93125e8b78baab9ea25eccd/src/Headshot.png"
                 alt="Profile"
                 className="w-full h-full rounded-full object-cover"
               />
@@ -134,8 +135,8 @@ function App() {
             <div className={`max-w-3xl mx-auto p-8 rounded-lg shadow-lg ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
               <div className="flex justify-end mb-6">
                 <a
-                  href="/src/merrittmason.pdf"
-                  target="/src/merrittmason.pdf"
+                  href="https://github.com/PartialArc/Resume-Website/blob/c97e04db5bc2f015f93125e8b78baab9ea25eccd/src/merrittmason.pdf"
+                  target="https://github.com/PartialArc/Resume-Website/blob/c97e04db5bc2f015f93125e8b78baab9ea25eccd/src/merrittmason.pdf"
                   className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700"
                 >
                   <Download size={16} className="mr-2" />
