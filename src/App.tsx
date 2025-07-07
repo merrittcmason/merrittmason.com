ed203fb3099a249d55c3b409a85cde9e22ceafe5
PartialArc <merrittcmason@gmail.com>
2025-07-07 01:10:30 -0400

    Fixed Various Typos in src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -116,7 +116,7 @@ function App() {
                 },
                                 {
                   title: "RoR Web Scraper",
-                  description: "RoB Web Scraper is a powerful tool designed to extract data from various websites quickly and efficiently.",
+                  description: "RoR Web Scraper is a powerful tool designed to extract data from various websites quickly and efficiently.",
                   imageUrl: "https://images.unsplash.com/photo-1522776851755-3914469f0ca2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                   link: "https://github.com/merrittcmason/Rob-Web-Scraper.git"
                 },
